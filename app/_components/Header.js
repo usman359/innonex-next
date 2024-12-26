4d670d5be317d02e7481b7a0d60069d97d5839ab
usman359 <m.usman095@gmail.com>
2024-12-26 17:52:15 +0500

    fix the issue in the header document number field

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -358,6 +358,40 @@ const Header = () => {
       customer.cardName?.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.ctrlKey && e.key.toLowerCase() === "f") {
+        e.preventDefault();
+        handleFindAction();
+
+        setctrlFEnterPressed(true); // Set the manual entry mode
+
+        // Reset document number field
+        setDocumentNumber(null);
+
+        if (docNumRef.current) {
+          docNumRef.current.focus(); // Set focus on the No. field
+          docNumRef.current.select(); // Highlight the text (if any)
+        }
+
+        // Reset the Series and Status fields to their first option
+        setSeries(""); // Set the Series to the first option
+        setStatus(""); // Reset Status to the default first option ("Open")
+        // setctrlFEnterPressed(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [
+    handleFindAction,
+    setSeries,
+    setStatus,
+    setDocumentNumber,
+    setctrlFEnterPressed,
+    docNumRef,
+  ]);
+
   const handleItemClick = (customer) => {
     setSelectedCustomer(customer); // Update selected customer
     setCustomerCodeSearchTerm(customer.cardCode); // Update the customer code search term
